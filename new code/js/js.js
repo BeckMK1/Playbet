2966972b0103b3dd6be6eabd75d891deafa2478d
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2019-10-29 12:26:05 +0100

    "bet" now id. add more js to "bet"

--- new code/js/js.js
+++ new code/js/js.js
@@ -4,10 +4,55 @@ $(document).ready(function(){
     var cost = 70;
     $("#total").html(full);
     $(".cost").html(cost);
-    $(".bet-AL").click(function(){
+    // bet function left
+    $("#bet-AL1").click(function(){
         $("#total").html(full-=cost);
+        $("#bet1").css("opacity", "10%")
     });
-
+    $("#bet-AL2").click(function(){
+        $("#total").html(full-=cost);
+        $("#bet2").css("opacity", "10%")
+    });
+    $("#bet-AL3").click(function(){
+        $("#total").html(full-=cost);
+        $("#bet3").css("opacity", "10%")
+    });
+    $("#bet-AL4").click(function(){
+        $("#total").html(full-=cost);
+        $("#bet4").css("opacity", "10%")
+    });
+    $("#bet-AL5").click(function(){
+        $("#total").html(full-=cost);
+        $("#bet5").css("opacity", "10%")
+    });
+// bet function right
+$("#bet-AR1").click(function(){
+    $("#total").html(full-=cost);
+    $("#bet1").css("opacity", "10%")
+    $("#bet-AL1").css("display", "none")
+    $("#bet-AR1").css("right","140px")
+    $("#bet-AR1").css("margin-top","40px")
+});
+$("#bet-AR2").click(function(){
+    $("#total").html(full-=cost);
+    $("#bet2").css("opacity", "10%")
+    $("#bet-AL2").css("display", "none")
+});
+$("#bet-AR3").click(function(){
+    $("#total").html(full-=cost);
+    $("#bet3").css("opacity", "10%")
+    $("#bet-AL3").css("display", "none")
+});
+$("#bet-AR4").click(function(){
+    $("#total").html(full-=cost);
+    $("#bet4").css("opacity", "10%")
+    $("#bet-AL4").css("display", "none")
+});
+$("#bet-AR5").click(function(){
+    $("#total").html(full-=cost);
+    $("#bet5").css("opacity", "10%")
+    $("#bet-AL5").css("display", "none")
+});
     // menu
     $('.sidebarbtn').click(function(){
         $('.sidebar').toggleClass('active')
